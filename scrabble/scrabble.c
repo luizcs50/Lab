1dd841658bb95a2bcb03efe6415ab7e74dedcc6f
luizcs50 <luiz.developer.acc@outlook.com>
2022-02-02 13:51:47 +0000

    scrablle made it

---
+++ scrabble/scrabble.c
@@ -0,0 +1,53 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+
+    if (score1 > score2)
+    {
+        printf("Player1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player2 wins!\n");
+    }
+    else {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+
+    int str_len = strlen(word);     // Holds the len of the string
+    int score = 0;      // Holds the score of the word
+    for (int i = 0; i < str_len; ++i)
+    {
+        if (isalpha(word[i]))       //  Check if the character is one of the alphabet
+        {
+            // Associate the characterof the word in uppercase - 65 to the index of POINTS
+            // Basic means, turn the char, eg: a in A, and subtract 65, turning in 0
+            score += POINTS[toupper(word[i]) - 65];
+        }
+    }
+
+    return score;
+}
